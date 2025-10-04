6b564f1c1f2e7df76227a4972070c9157b305e7c
Yinson Tso <yinson.tso@gmail.com>
2025-10-04 10:56:50 -0400

    Added position grabbing

--- src/map.jsx
+++ src/map.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
@@ -12,11 +12,29 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
+// Component that listens for clicks
+function ClickMarker({ onClick }) {
+  useMapEvents({
+    click(e) {
+      const { lat, lng } = e.latlng;
+      onClick(lat, lng); // call the callback from props
+      console.log("Clicked coordinates:", lat, lng);
+    },
+  });
+  return null; // This component renders nothing
+}
+
 const Map = () => {
+  const [coords, setCoords] = useState(null);
+
+  const handleClick = (lat, lng) => {
+    setCoords([lat, lng]);
+  };
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <MapContainer
-        center={[0,0]}
+        center={[0, 0]}
         zoom={2}
         style={{ height: "100%", width: "100%" }}
         scrollWheelZoom={true}
@@ -25,15 +43,22 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <Marker position={[40.7128, -74.006]}>
-          <Popup>
-            Hello from New York! <br /> Zoom or drag the map freely.
-          </Popup>
-        </Marker> */}
+
+        {/* Capture clicks */}
+        <ClickMarker onClick={handleClick} />
+
+        {/* Drop a marker at the clicked point */}
+        {coords && <Marker position={coords} />}
+
+        {/* 
+        <Marker position={[40.7128, -74.006]}> 
+            <Popup> Hello from New York! <br /> Zoom or drag the map freely. 
+            </Popup> 
+        </Marker> 
+        */}
       </MapContainer>
     </div>
   );
-}
-
+};
 
 export default Map;
